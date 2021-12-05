6344ae967570948a7ddeed6e64cca9e253d55072
speed1313 <speedtry13@icloud.com>
2021-12-06 01:06:27 +0900

    impl description message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+
+
+// called when we are passed a markdown file via command line
+fn parse_markdown_file(){
+
 }
+// this will output the title, version, and description
+fn print_short_banner(){
+    println!("{}", get_title());
+}
+
+//this will output the short banner plus a "written by" plus "usage"
+fn print_long_banner(){
+    print_short_banner();
+      println!("Written by: {}\nHomepage: {}\nUsage: tinymd <somefile>.md\n",
+    env!("CARGO_PKG_AUTHORS"),
+    env!("CARGO_PKG_HOMEPAGE")
+  );
+}
+
+fn get_title() -> String{
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v)");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str(")");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    return the_title;
+}
+fn main() {
+   print_short_banner(); 
+}
+
